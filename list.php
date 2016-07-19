62bc88dca8c8172bd7f24d6e95ea09464719cb58
soraciel <ehem_nuning@yahoo.co.id>
2016-07-19 09:15:50 +0700

    wkwkwk

---
+++ list.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CRUD codeigniter 3 - belajarphp.net</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+        <table class="table table-bordered">
+            <tr><th>NIS</th><th>NAMA</th><th>JENIS KELAMIN</th><th colspan="2">OPERASI</th></tr>
+            <?php
+            foreach ($siswas->result() as $siswa) {
+            $gender = $siswa->gender == 'F' ? 'Perempuan' : 'Laki Laki';
+            echo "<tr>
+            <td>$siswa->nis</td>
+            <td>$siswa->nama</td>
+            <td>$gender</td>
+            <td></td>
+            </tr>";
+            }
+            ?>
+        </table>
+    </body>
+</html>
